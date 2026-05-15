0386d48535ac42105c918a2af2b62e91ebb41772
chriswaldron15 <chriswaldron15@gmail.com>
2026-05-15 09:00:18 +0100

    Spline current improvements

--- Assets/Scripts/Water/SplineCurrent.cs
+++ Assets/Scripts/Water/SplineCurrent.cs
@@ -11,12 +11,28 @@ namespace BoatGame
         [SerializeField] private float force;
         public float Force => force;
         [SerializeField] private SplineContainer spline;
-        [SerializeField] private float splineWidth;
+        [SerializeField, Range(0, 10)] private float splineWidth;
         public float SplineWidth => splineWidth;
         [SerializeField] private int splineResolution = 1;
         
         private readonly HashSet<Rigidbody> _targets = new();
         private NativeSpline _nativeSpline;
+
+        private void OnValidate()
+        {
+            if (spline == null) return;
+            var sphere = GetComponent<SphereCollider>();
+            if (sphere == null) return;
+
+            if (spline.Spline.Count == 0)
+                return;
+            
+            Bounds bounds = spline.Spline.GetBounds();
+            bool first = true;
+                sphere.center = bounds.center;
+                sphere.radius = bounds.extents.magnitude + splineWidth;
+                sphere.isTrigger = true;
+        }
 
         private void Awake()
         {
@@ -31,13 +47,16 @@ namespace BoatGame
         private void FixedUpdate()
         {
             var forceThisFrame = force * Time.fixedDeltaTime;
+            var myPos = transform.position;
+            var myPosf3 = new float3(myPos);
 
             foreach (var rb in _targets)
             {
-                if (SplineUtility.GetNearestPoint(_nativeSpline, new float3(rb.position), out var nearest, out float t, splineResolution, 1) < splineWidth)
+                if (SplineUtility.GetNearestPoint(_nativeSpline, new float3(rb.position - myPos), out var nearest, out float t, splineResolution) < splineWidth && t > 0 && t < 0.99f)
                 {
-                    var nextPos = _nativeSpline.EvaluatePosition(t + 0.01f);
-                    var dir = (Vector3)nextPos - rb.position;
+                    var nextPos = _nativeSpline.EvaluatePosition(t + 0.1f) + myPosf3;
+                    var nearestWorld = nearest + myPosf3;
+                    var dir = (Vector3)(nextPos - nearestWorld);
                     dir.Normalize();
                     rb.AddForce(dir * forceThisFrame, ForceMode.Acceleration);
                 }
